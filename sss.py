64c2f2eed9ed23aa7fb5d63b251d80cbcc0f7ab9
Daniel Pellarini <daniel.pellarini@gmail.com>
2020-06-17 17:27:37 +0200

    Switched to using object arrays to avoid overflows in numpy

--- sss.py
+++ sss.py
@@ -1,7 +1,7 @@
-'''
+"""
 An implementation of the Shamir secret sharing scheme
 https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing
-'''
+"""
 
 import secrets
 import numpy as np
@@ -9,12 +9,12 @@ import sympy
 
 
 class SSS:
-    '''
+    """
         k = the key
         p = prime number for modulo operations in the set Z_p
         w = number of shares
         t = minimum number of shares required to reconstruct the key k
-    '''
+    """
 
     def __init__(self, k, p, w, t):
         if not (sympy.isprime(p)):
@@ -65,8 +65,11 @@ class SSS:
         y = []
         self.choose_a()
         for idx, element in enumerate(range(0, self.w)):
-            ax = np.poly1d(self.a + [self.k])  # ax is the polynomial we create before computing y_i
-            y.append(ax(self.x[idx]) % self.p)  # evaluate the polynomial to generate y_i values
+            #ax = np.poly1d(self.a + [self.k])  # ax is the polynomial we create before computing y_i
+            poly = np.array(self.a + [self.k], dtype=object)
+            ecs = np.array(self.x[idx], dtype=object)
+            y.append(np.polyval(poly, ecs) % self.p)
+            #y.append(ax(self.x[idx]) % self.p)  # evaluate the polynomial to generate y_i values
         self.y = y
         return self.y
 
@@ -139,21 +142,22 @@ class SSS:
 sss = SSS(13, 17, 5, 3)
 print("x = " + str(sss.choose_x()))
 print("y = " + str(sss.generate_shares()))
-
 # manual test
 print("The key is " + str(sss.reconstruct_key([1, 3, 5], [8, 10, 11]))) # must return 13
 
 sss = SSS(1234, 1613, 6, 3)
 print("x = " + str(sss.choose_x()))
 print("y = " + str(sss.generate_shares()))
-
 # manual test
 print("The key is " + str(sss.reconstruct_key([1, 2, 3], [1494, 329, 965])))  # must return 1234
 
-sss = SSS(1234, 31847, 10, 5)
+sss = SSS(12341, 31847, 10, 5)
+print("x = " + str(sss.choose_x()))
+print("y = " + str(sss.generate_shares()))
 # manual test
 print(sss.reconstruct_key([413, 432, 451, 470, 489], [25439, 14847, 24780, 5910, 12734]))
 print(sss.reconstruct_key([584, 432, 451, 470, 489], [21462, 14847, 24780, 5910, 12734]))
 print(sss.reconstruct_key([584, 413, 565, 546, 489], [21462, 25439, 20806, 28578, 12734]))
 print(sss.reconstruct_key([489, 565, 451, 470, 527], [12734, 20806, 24780, 5910, 12555]))
 print(sss.reconstruct_key([508, 432, 584, 470, 489], [12492, 14847, 21462, 5910, 12734]))
+print(sss.reconstruct_key([27644, 28653, 13898, 20938, 20988], [10324, 11432, 16976, 2126, 9125]))
