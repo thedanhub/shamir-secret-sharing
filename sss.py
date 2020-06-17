d013df4c0ba7f0fb329f6966b9e829a4a0c3bc1c
Daniel Pellarini <daniel.pellarini@gmail.com>
2020-06-17 18:33:51 +0200

    Cleanup

--- sss.py
+++ sss.py
@@ -65,11 +65,9 @@ class SSS:
         y = []
         self.choose_a()
         for idx, element in enumerate(range(0, self.w)):
-            #ax = np.poly1d(self.a + [self.k])  # ax is the polynomial we create before computing y_i
             poly = np.array(self.a + [self.k], dtype=object)
             ecs = np.array(self.x[idx], dtype=object)
             y.append(np.polyval(poly, ecs) % self.p)
-            #y.append(ax(self.x[idx]) % self.p)  # evaluate the polynomial to generate y_i values
         self.y = y
         return self.y
 
@@ -143,7 +141,7 @@ sss = SSS(13, 17, 5, 3)
 print("x = " + str(sss.choose_x()))
 print("y = " + str(sss.generate_shares()))
 # manual test
-print("The key is " + str(sss.reconstruct_key([1, 3, 5], [8, 10, 11]))) # must return 13
+print("The key is " + str(sss.reconstruct_key([1, 3, 5], [8, 10, 11])))  # must return 13
 
 sss = SSS(1234, 1613, 6, 3)
 print("x = " + str(sss.choose_x()))
@@ -151,7 +149,7 @@ print("y = " + str(sss.generate_shares()))
 # manual test
 print("The key is " + str(sss.reconstruct_key([1, 2, 3], [1494, 329, 965])))  # must return 1234
 
-sss = SSS(12341, 31847, 10, 5)
+sss = SSS(30000, 31847, 10, 5)
 print("x = " + str(sss.choose_x()))
 print("y = " + str(sss.generate_shares()))
 # manual test
@@ -160,4 +158,3 @@ print(sss.reconstruct_key([584, 432, 451, 470, 489], [21462, 14847, 24780, 5910,
 print(sss.reconstruct_key([584, 413, 565, 546, 489], [21462, 25439, 20806, 28578, 12734]))
 print(sss.reconstruct_key([489, 565, 451, 470, 527], [12734, 20806, 24780, 5910, 12555]))
 print(sss.reconstruct_key([508, 432, 584, 470, 489], [12492, 14847, 21462, 5910, 12734]))
-print(sss.reconstruct_key([27644, 28653, 13898, 20938, 20988], [10324, 11432, 16976, 2126, 9125]))
